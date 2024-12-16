68e039033d4d7f9fa7e535b1b3afdff1b72a879e
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-12-16 01:57:55 -0300

    GET Department by id

--- src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
+++ src/main/java/com/devsuperior/workshopcassandra/controllers/DepartmentController.java
@@ -1,12 +1,15 @@
 package com.devsuperior.workshopcassandra.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.devsuperior.workshopcassandra.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.services.DepartmentService;
@@ -23,4 +26,13 @@ public class DepartmentController {
 		List<DepartmentDTO> list=service.findAll();
 		return ResponseEntity.ok(list);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<DepartmentDTO> findById(@PathVariable UUID id){
+		DepartmentDTO obj=service.findById(id);
+		return ResponseEntity.ok(obj);
+	}
+	
+	
+	
 }
